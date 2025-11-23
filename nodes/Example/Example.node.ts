9b223c2070a68c5b651d8101b74d220e1dc76ae0
Lukas Bombach <lukas.bombach@googlemail.com>
2025-11-23 16:18:39 +0100

    tagging works

--- nodes/Example/Example.node.ts
+++ nodes/Example/Example.node.ts
@@ -4,7 +4,7 @@ import type {
 	INodeType,
 	INodeTypeDescription,
 } from 'n8n-workflow';
-import { NodeConnectionTypes, NodeOperationError } from 'n8n-workflow';
+import { NodeConnectionTypes } from 'n8n-workflow';
 import { run } from '@bufbuild/cel';
 import { STRINGS_EXT_FUNCS } from '@bufbuild/cel/ext/strings';
 
@@ -19,13 +19,13 @@ type Rule = {
 
 export class Example implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Example',
+		displayName: 'Tag Transactions',
 		name: 'example',
 		icon: { light: 'file:example.svg', dark: 'file:example.dark.svg' },
 		version: 1,
-		description: 'Basic Example Node',
+		description: 'Tag transactions based on rules',
 		defaults: {
-			name: 'Example',
+			name: 'Tag Transactions',
 		},
 		usableAsTool: true,
 		group: ['transform'],
@@ -46,73 +46,17 @@ export class Example implements INodeType {
 		properties: [],
 	};
 
-	// The function below is responsible for actually doing whatever this node
-	// is supposed to do. You can make async calls and use `await`.
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const rules = this.getInputData(0).map((item) => item.json) as Rule[];
 		const data = this.getInputData(1).map((item) => item.json) as Transaction[];
 
 		return [
 			data.map((item) => {
-				/* const tag = run(item.rule, { text: 'edeka obi' }, { funcs: STRINGS_EXT_FUNCS })
-					? item.tag
-					: null; */
-
 				const tags = rules
 					.filter(({ rule }) => run(rule, item, { funcs: STRINGS_EXT_FUNCS }))
 					.map(({ tag }) => tag);
-
 				return { json: { ...item, tags } };
 			}),
 		];
-
-		/* const items = this.getInputData();
-
-		const result: INodeExecutionData[] = [];
-
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-			try {
-				const item = items[itemIndex];
-				const data = item.json;
-
-				if (!data.tag || typeof data.tag !== 'string') {
-					throw new NodeOperationError(
-						this.getNode(),
-						'Missing or invalid required field: "tag" must be a string',
-						{ itemIndex },
-					);
-				}
-
-				if (!data.rule || typeof data.rule !== 'string') {
-					throw new NodeOperationError(
-						this.getNode(),
-						'Missing or invalid required field: "rule" must be a string',
-						{ itemIndex },
-					);
-				}
-
-				const validatedData = data as InputItem;
-
-				const tag = run(validatedData.rule, { text: 'edeka obi' }, { funcs: STRINGS_EXT_FUNCS })
-					? validatedData.tag
-					: null;
-
-				result.push({ json: { tag } });
-			} catch (error) {
-				if (this.continueOnFail()) {
-					items.push({ json: { error: error.message }, pairedItem: itemIndex });
-				} else {
-					if (error.context) {
-						error.context.itemIndex = itemIndex;
-						throw error;
-					}
-					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
-					});
-				}
-			}
-		}
-
-		return [result]; */
 	}
 }
